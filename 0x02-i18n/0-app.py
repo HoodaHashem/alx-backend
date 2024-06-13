641735e9ead621982bf7c29a3b49554bedf33a81
HoodaHashem <mahmoudhashem335@gmail.com>
2024-06-13 18:20:40 +0300

    Styling

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,7 +1,12 @@
+#!/usr/bin/env python3
+
+""" Basic Babel setup """
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
+    """ Main route """
     return render_template('0-index.html')
