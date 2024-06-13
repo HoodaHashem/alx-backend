e362abc191740546cf984edaa9640c1e8b54e2fb
HoodaHashem <mahmoudhashem335@gmail.com>
2024-06-13 19:57:33 +0300

    Styling

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -11,13 +11,16 @@ class Config(object):
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
+
 app = Flask(__name__)
 babel = Babel(app)
+
 
 @app.route('/')
 def index():
     """ index route """
     return render_template('1-index.html')
+
 
 if __name__ == "__main__":
     app.run(port=5000, debug=True, host='0.0.0.0')
